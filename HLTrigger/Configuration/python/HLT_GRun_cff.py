24ae3c0bc39b4a05607d4fb4e587f045a4bef51a
Martin Grunewald <Martin.Grunewald@cern.ch>
2018-11-12 10:07:50 +0100

    Toward HIon HLT menu V1.4

--- HLTrigger/Configuration/python/HLT_GRun_cff.py
+++ HLTrigger/Configuration/python/HLT_GRun_cff.py
@@ -1,13 +1,13 @@
 # hltGetConfiguration --cff --data /dev/CMSSW_10_3_0/GRun --type GRun
 
-# /dev/CMSSW_10_3_0/GRun/V33 (CMSSW_10_3_0)
+# /dev/CMSSW_10_3_0/GRun/V34 (CMSSW_10_3_0)
 
 import FWCore.ParameterSet.Config as cms
 
 fragment = cms.ProcessFragment( "HLT" )
 
 fragment.HLTConfigVersion = cms.PSet(
-  tableName = cms.string('/dev/CMSSW_10_3_0/GRun/V33')
+  tableName = cms.string('/dev/CMSSW_10_3_0/GRun/V34')
 )
 
 fragment.transferSystem = cms.PSet( 
