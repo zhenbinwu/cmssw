7af6e5974458feae380e1a2d41c0ba018fbdd24b
Sioni Summers <ss5310@ic.ac.uk>
2021-03-08 18:00:04 +0100

    Make L1SeedConePFJetEmulator standalone

--- L1Trigger/Phase2L1ParticleFlow/plugins/L1SeedConePFJetProducer.cc
+++ L1Trigger/Phase2L1ParticleFlow/plugins/L1SeedConePFJetProducer.cc
@@ -14,8 +14,7 @@
 #include "DataFormats/Math/interface/deltaR.h"
 
 // bitwise emulation headers
-#include "ap_int.h"
-#include "ap_fixed.h"
+#include "L1Trigger/Phase2L1ParticleFlow/interface/L1SeedConePFJetEmulator.h"
 
 class L1SeedConePFJetProducer : public edm::global::EDProducer<> {
 public:
@@ -38,7 +37,9 @@ private:
   std::vector<l1t::PFJet> processEvent_HW(std::vector<edm::Ptr<l1t::PFCandidate>>& parts) const;
 
   l1t::PFJet makeJet_SW(const std::vector<edm::Ptr<l1t::PFCandidate>>& parts) const;
-  l1t::PFJet makeJet_HW(const std::vector<edm::Ptr<l1t::PFCandidate>>& parts) const;
+
+  static std::vector<L1SCJetEmu::Particle> convertEDMToHW(std::vector<edm::Ptr<l1t::PFCandidate>>& edmParticles);
+  static std::vector<l1t::PFJet> convertHWToEDM(std::vector<L1SCJetEmu::Jet> hwJets);
 };
 
 L1SeedConePFJetProducer::L1SeedConePFJetProducer(const edm::ParameterSet& cfg)
@@ -141,231 +142,36 @@ std::vector<l1t::PFJet> L1SeedConePFJetProducer::processEvent_SW(std::vector<edm
   return jets;
 }
 
-namespace L1SCJetEmu {
-  // Data types and constants used in the FPGA and FPGA-optimized functions
-
-  //etaphi_base maps physical eta phi units onto bits
-  //This way, the least significant bit of etaphi_t is exactly 0.01
-  //Even though 0.01 is not a power of 2
-  static float etaphi_base = 100. / 64;
-  typedef ap_ufixed<16, 14> pt_t;        // 1 unit = 0.25 GeV;
-  typedef ap_fixed<10, 4> etaphi_t;      // 1 unit = 0.01;
-  typedef ap_fixed<12, 6> detaphi_t;     // type for the difference between etas or phis
-  typedef ap_fixed<18, 9> detaphi2_t;    // type for detaphi_t squared
-  typedef ap_fixed<22, 16> pt_etaphi_t;  // type for product of pt with deta or phi
-  typedef ap_uint<5> count_t;            // type for multiplicity
-
-  // constants for the axis update
-  typedef ap_ufixed<18, -2> inv_pt_t;
-  static constexpr int N_table_inv_pt = 1024;
-
-  static const detaphi_t TWOPI = 3.14159 * 2. * etaphi_base;
-  static const detaphi_t PI = 3.14159 * etaphi_base;
-  static const detaphi_t HALFPI = 3.14159 / 2 * etaphi_base;
-  //static const detaphi_t RCONE = 0.4 * 100 / 128;
-  //static const detaphi_t R2CONE = RCONE * RCONE;
-  //
-  static const etaphi_t FIDUCIAL_ETA_PHI = 5.11 * etaphi_base;
-  //static const etaphi_t FIDUCIAL_ETA_PHI = 5.11;
-  static const pt_t JET_PT_CUT = 5;
-
-  constexpr int ceillog2(int x) { return (x <= 2) ? 1 : 1 + ceillog2((x + 1) / 2); }
-
-  constexpr int floorlog2(int x) { return (x < 2) ? 0 : 1 + floorlog2(x / 2); }
-
-  constexpr int pow2(int x) { return x == 0 ? 1 : 2 * pow2(x - 1); }
-
-  template <class data_T, int N>
-  inline float real_val_from_idx(unsigned i) {
-    // Treat the index as the top N bits
-    static constexpr int NB = ceillog2(N);  // number of address bits for table
-    data_T x(0);
-    // The MSB of 1 is implicit in the table
-    x[x.width - 1] = 1;
-    // So we can use the next NB bits for real data
-    x(x.width - 2, x.width - NB - 1) = i;
-    return (float)x;
-  }
-
-  template <class data_T, int N>
-  inline unsigned idx_from_real_val(data_T x) {
-    // Slice the top N bits to get an index into the table
-    static constexpr int NB = ceillog2(N);  // number of address bits for table
-    // Slice the top-1 NB bits of the value
-    // the MSB of '1' is implicit, so only slice below that
-    ap_uint<NB> y = x(x.width - 2, x.width - NB - 1);
-    return (unsigned)y(NB - 1, 0);
-  }
-
-  template <class data_T, class table_T, int N>
-  void init_invert_table(table_T table_out[N]) {
-    // The template data_T is the data type used to address the table
-    for (unsigned i = 0; i < N; i++) {
-      float x = real_val_from_idx<data_T, N>(i);
-      table_T inv_x = 1 / x;
-      table_out[i] = inv_x;
-    }
-  }
-
-  template <class in_t, class table_t, int N>
-  table_t invert_with_shift(in_t in, bool debug = false) {
-    table_t inv_table[N];
-    init_invert_table<in_t, table_t, N>(inv_table);
-
-    // find the first '1' in the denominator
-    int msb = 0;
-    for (int b = 0; b < in.width; b++) {
-      if (in[b])
-        msb = b;
-    }
-    // shift up the denominator such that the left-most bit (msb) is '1'
-    in_t in_shifted = in << (in.width - msb - 1);
-    // lookup the inverse of the shifted input
-    int idx = idx_from_real_val<in_t, N>(in_shifted);
-    table_t inv_in = inv_table[idx];
-    // shift the output back
-    table_t out = inv_in << (in.width - msb - 1);
-    if (debug) {
-      std::cout << "           x " << in << ", msb = " << msb << ", shift = " << (in.width - msb) << ", idx = " << idx
-                << std::endl;
-      std::cout << "     pre 1 / " << in_shifted << " = " << inv_in << "(" << 1 / (float)in_shifted << ")" << std::endl;
-      std::cout << "    post 1 / " << in << " = " << out << "(" << 1 / (float)in << ")" << std::endl;
-    }
-    return out;
-  }
-
-  detaphi_t deltaPhi(edm::Ptr<l1t::PFCandidate> a, edm::Ptr<l1t::PFCandidate> b) {
-    // scale the particle eta, phi to hardware units
-    etaphi_t aphi = etaphi_t(a->phi() * etaphi_base);
-    etaphi_t bphi = etaphi_t(b->phi() * etaphi_base);
-    detaphi_t dphi = detaphi_t(aphi) - detaphi_t(bphi);
-    // phi wrap
-    detaphi_t dphi0 = dphi > PI ? detaphi_t(TWOPI - dphi) : detaphi_t(dphi);
-    detaphi_t dphi1 = dphi < -PI ? detaphi_t(TWOPI + dphi) : detaphi_t(dphi);
-    detaphi_t dphiw = dphi > detaphi_t(0) ? dphi0 : dphi1;
-    return dphiw;
-  }
-
-  bool inCone(edm::Ptr<l1t::PFCandidate> seed, edm::Ptr<l1t::PFCandidate> part, detaphi_t cone2, bool debug) {
-    // scale the particle eta, phi to hardware units
-    etaphi_t seta = etaphi_t(seed->eta() * etaphi_base);
-    etaphi_t peta = etaphi_t(part->eta() * etaphi_base);
-    detaphi_t deta = detaphi_t(seta) - detaphi_t(peta);
-    detaphi_t dphi = deltaPhi(seed, part);
-    bool ret = deta * deta + dphi * dphi < cone2;
-    //bool ret = r2 < cone2;
-    if (debug) {
-      detaphi2_t r2 = detaphi2_t(deta) * detaphi2_t(deta) + detaphi2_t(dphi) * detaphi2_t(dphi);
-      std::cout << "  part eta, seed eta: " << etaphi_t(part->eta() * etaphi_base) << ", "
-                << etaphi_t(seed->eta() * etaphi_base) << std::endl;
-      std::cout << "  part phi, seed phi: " << etaphi_t(part->phi() * etaphi_base) << ", "
-                << etaphi_t(seed->phi() * etaphi_base) << std::endl;
-      std::cout << "  pt, deta, dphi, r2, lt: " << part->pt() << ", " << deta << ", " << dphi << ", " << r2 << ", "
-                << ret << std::endl;
-    }
-    return ret;
-  }
-};  // namespace L1SCJetEmu
-
-l1t::PFJet L1SeedConePFJetProducer::makeJet_HW(const std::vector<edm::Ptr<l1t::PFCandidate>>& parts) const {
-  // Seed Cone Jet algorithm with ap_fixed types and hardware emulation
+std::vector<l1t::PFJet> L1SeedConePFJetProducer::processEvent_HW(std::vector<edm::Ptr<l1t::PFCandidate>>& work) const {
+  // The fixed point emulator
+  // Convert the EDM format to the hardware format, and call the standalone emulator
   using namespace L1SCJetEmu;
-  edm::Ptr<l1t::PFCandidate> seed = parts.at(0);
-
-  // Fine unless we start using saturation, in which case order matters
-  auto sumpt = [](pt_t(a), const edm::Ptr<l1t::PFCandidate>& b) { return a + (pt_t)b->pt(); };
-
-  // Sum the pt
-  pt_t pt = std::accumulate(parts.begin(), parts.end(), pt_t(0), sumpt);
-  inv_pt_t inv_pt = invert_with_shift<pt_t, inv_pt_t, N_table_inv_pt>(pt, false);
-
-  // pt weighted d eta
-  std::vector<pt_etaphi_t> pt_deta;
-  pt_deta.resize(parts.size());
-  std::transform(parts.begin(), parts.end(), pt_deta.begin(), [&seed, &pt](const edm::Ptr<l1t::PFCandidate>& part) {
-    // In the firmware we calculate the per-particle pt-weighted deta
-    // Scale the eta, coordinate to the HW system
-    return pt_etaphi_t(pt_t(part->pt()) *
-                       detaphi_t(etaphi_t(part->eta() * etaphi_base) - etaphi_t(seed->eta() * etaphi_base)));
-  });
-  // Accumulate the pt-weighted etas. Init to 0, start accumulating at begin()+1 to skip seed
-  pt_etaphi_t sum_pt_eta = std::accumulate(pt_deta.begin() + 1, pt_deta.end(), pt_etaphi_t(0));
-  etaphi_t eta = etaphi_t(seed->eta() * etaphi_base) + etaphi_t(sum_pt_eta * inv_pt);
-
-  // pt weighted d phi
-  std::vector<pt_etaphi_t> pt_dphi;
-  pt_dphi.resize(parts.size());
-  std::transform(parts.begin(), parts.end(), pt_dphi.begin(), [&seed, &pt](const edm::Ptr<l1t::PFCandidate>& part) {
-    // In the firmware we calculate the per-particle pt-weighted dphi
-    return pt_etaphi_t(pt_t(part->pt()) * detaphi_t(deltaPhi(part, seed)));
-  });
-  // Accumulate the pt-weighted etas. Init to 0, start accumulating at begin()+1 to skip seed
-  pt_etaphi_t sum_pt_phi = std::accumulate(pt_dphi.begin() + 1, pt_dphi.end(), pt_etaphi_t(0));
-  etaphi_t phi = etaphi_t(seed->phi() * etaphi_base) + etaphi_t(sum_pt_phi * inv_pt);
-
-  l1t::PFJet jet(pt, float(eta) / etaphi_base, float(phi) / etaphi_base);
-  for (auto it = parts.begin(); it != parts.end(); it++) {
-    jet.addConstituent(*it);
-  }
-
-  if (_debug) {
-    std::for_each(pt_dphi.begin(), pt_dphi.end(), [](pt_etaphi_t& x) { std::cout << "pt_dphi: " << x << std::endl; });
-    std::for_each(pt_deta.begin(), pt_deta.end(), [](pt_etaphi_t& x) { std::cout << "pt_deta: " << x << std::endl; });
-    std::cout << " sum_pt_eta: " << sum_pt_eta << ", sum_pt_eta * 1/pt: " << etaphi_t(sum_pt_eta * inv_pt) << std::endl;
-    std::cout << " sum_pt_phi: " << sum_pt_phi << ", sum_pt_phi * 1/pt: " << etaphi_t(sum_pt_phi * inv_pt) << std::endl;
-    std::cout << " uncorr eta: " << etaphi_t(seed->eta() * etaphi_base)
-              << ", phi: " << etaphi_t(seed->phi() * etaphi_base) << std::endl;
-    std::cout << "   corr eta: " << eta << ", phi: " << phi << std::endl;
-    std::cout << "         pt: " << pt << std::endl;
-  }
-
-  return jet;
+  Config config(_debug, _coneSize, _nJets);
+  std::vector<Particle> particles = convertEDMToHW(work);
+  std::vector<Jet> jets = emulateEvent(particles, config);
+  return convertHWToEDM(jets);
 }
 
-std::vector<l1t::PFJet> L1SeedConePFJetProducer::processEvent_HW(std::vector<edm::Ptr<l1t::PFCandidate>>& parts) const {
-  // The fixed point algorithm emulation
+std::vector<L1SCJetEmu::Particle> L1SeedConePFJetProducer::convertEDMToHW(std::vector<edm::Ptr<l1t::PFCandidate>>& edmParticles){
   using namespace L1SCJetEmu;
-  std::vector<edm::Ptr<l1t::PFCandidate>> work;
-  work.resize(parts.size());
-  std::transform(parts.begin(), parts.end(), work.begin(), [](const edm::Ptr<l1t::PFCandidate>& part) { return part; });
-  std::sort(work.begin(), work.end(), [](edm::Ptr<l1t::PFCandidate> i, edm::Ptr<l1t::PFCandidate> j) {
-    return (i->pt() > j->pt());
+  std::vector<Particle> hwParticles;
+  std::for_each(edmParticles.begin(), edmParticles.end(), [&](edm::Ptr<l1t::PFCandidate>& edmParticle){
+    hwParticles.push_back(L1SCJetEmu::Particle(edmParticle->pt(),
+                                               edmParticle->eta() * etaphi_base,
+                                               edmParticle->phi() * etaphi_base));
   });
-  // It would be nice to transform the inputs to the etaphi_base of the FW here, as in the line below
-  // However the phi may wrap around if the etaphi_base > 1, so don't do it...
-  //std::for_each(work.begin(), work.end(), [](l1t::PFCandidate& x){x.setP4(math::PtEtaPhiMLorentzVector(pt_t(x.pt()), etaphi_t(x.eta()*etaphi_base), etaphi_t(x.phi()*etaphi_base), x.mass()));});
+  return hwParticles;
+}
 
-  detaphi_t rCone2 = detaphi_t(_coneSize * _coneSize * etaphi_base * etaphi_base);
-
-  std::vector<l1t::PFJet> jets;
-  jets.reserve(_nJets);
-  while (!work.empty() && jets.size() < _nJets) {
-    // Take the first (highest pt) candidate as a seed
-    edm::Ptr<l1t::PFCandidate> seed = work.at(0);
-    // Get the particles within a _coneSize of the seed
-    std::vector<edm::Ptr<l1t::PFCandidate>> particlesInCone;
-    std::copy_if(
-        work.begin(), work.end(), std::back_inserter(particlesInCone), [&](const edm::Ptr<l1t::PFCandidate>& part) {
-          return inCone(seed, part, rCone2, false);
-        });
-    if (_debug) {
-      std::cout << "Seed: " << pt_t(seed->pt()) << ", " << etaphi_t(seed->eta() * etaphi_base) << ", "
-                << etaphi_t(seed->phi() * etaphi_base) << std::endl;
-      std::for_each(particlesInCone.begin(), particlesInCone.end(), [&](edm::Ptr<l1t::PFCandidate>& part) {
-        std::cout << "  Part: " << pt_t(part->pt()) << ", " << etaphi_t(part->eta() * etaphi_base) << ", "
-                  << etaphi_t(part->phi() * etaphi_base) << std::endl;
-        inCone(seed, part, rCone2, true);
-      });
-    }
-    jets.push_back(makeJet_HW(particlesInCone));
-    // remove the clustered particles
-    work.erase(
-        std::remove_if(work.begin(),
-                       work.end(),
-                       [&](const edm::Ptr<l1t::PFCandidate>& part) { return inCone(seed, part, rCone2, false); }),
-        work.end());
-  }
-  return jets;
+std::vector<l1t::PFJet> L1SeedConePFJetProducer::convertHWToEDM(std::vector<L1SCJetEmu::Jet> hwJets){
+  using namespace L1SCJetEmu;
+  std::vector<l1t::PFJet> edmJets;
+  std::for_each(hwJets.begin(), hwJets.end(), [&](Jet jet){
+    edmJets.push_back(l1t::PFJet(jet.hwPt,
+                                 float(jet.hwEta) / etaphi_base,
+                                 float(jet.hwPhi) / etaphi_base));
+  });
+  return edmJets;
 }
 
 #include "FWCore/Framework/interface/MakerMacros.h"
