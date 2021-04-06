f36d29593d78dea3924fbc32c5bfef9567574d44
jheikkil <jaana.kristiina.heikkilae@cern.ch>
2021-04-06 15:10:05 +0200

    Adding new objects

--- L1Trigger/L1CaloTrigger/python/L1CaloJetHTTProducer_cfi.py
+++ L1Trigger/L1CaloTrigger/python/L1CaloJetHTTProducer_cfi.py
@@ -5,6 +5,6 @@ L1CaloJetHTTProducer = cms.EDProducer("L1CaloJetHTTProducer",
     PtMin = cms.double(30.0),
     BXVCaloJetsInputTag = cms.InputTag("L1CaloJetProducer","L1CaloJetCollectionBXV"),
     genJets = cms.InputTag("ak4GenJetsNoNu", "", "HLT"),
-    debug = cms.bool(False),
+    debug = cms.bool(True),
     use_gen_jets = cms.bool(False),
 )
