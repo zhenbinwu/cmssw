f0665f34629044f12aba04f5be1ded780a49a9b0
Sioni Summers <ss5310@ic.ac.uk>
2021-11-05 13:04:45 +0100

    Make more parameters configurable, pad output link with valid but empty jets up to constant nJets (12 by default)

--- L1Trigger/Phase2L1ParticleFlow/plugins/L1CTJetFileWriter.cc
+++ L1Trigger/Phase2L1ParticleFlow/plugins/L1CTJetFileWriter.cc
@@ -29,16 +29,12 @@ public:
 
 private:
   // ----------constants, enums and typedefs ---------
-  // NOTE: At least some of the info from these constants will eventually come from config files
-  static constexpr size_t kFramesPerTMUXPeriod = 9;
-  static constexpr size_t kGapLengthOutput = 54-2*10;
-  static constexpr size_t kCTL2BoardTMUX = 6;
-  static constexpr size_t kMaxLinesPerFile = 1024;
-
-  const std::map<l1t::demo::LinkId, std::pair<l1t::demo::ChannelSpec, std::vector<size_t>>>
-      kChannelSpecsOutputToGT = {
-          /* logical channel within time slice -> {{link TMUX, inter-packet gap}, vector of channel indices} */
-          {{"jets", 0}, {{kCTL2BoardTMUX, kGapLengthOutput}, {0}}}};
+  unsigned nJets;
+  size_t kFramesPerBX; 
+  size_t kCTL2BoardTMUX; 
+  size_t kGapLengthOutput; 
+  size_t kMaxLinesPerFile; 
+  std::map<l1t::demo::LinkId, std::pair<l1t::demo::ChannelSpec, std::vector<size_t>>> kChannelSpecsOutputToGT; 
 
   // ----------member functions ----------------------
   void analyze(const edm::Event&, const edm::EventSetup&) override;
@@ -50,13 +46,20 @@ private:
 };
 
 L1CTJetFileWriter::L1CTJetFileWriter(const edm::ParameterSet& iConfig) :
+      nJets(iConfig.getParameter<unsigned>("nJets")),
+      kFramesPerBX(iConfig.getParameter<unsigned>("nFramesPerBX")),
+      kCTL2BoardTMUX(iConfig.getParameter<unsigned>("TMUX")),
+      kGapLengthOutput(kFramesPerBX - 2 * nJets),
+      kMaxLinesPerFile(iConfig.getParameter<unsigned>("maxLinesPerFile")),
+      kChannelSpecsOutputToGT{{{"jets", 0}, {{kCTL2BoardTMUX, kGapLengthOutput}, {0}}}},
       jetsToken_(consumes<edm::View<l1t::PFJet>>(iConfig.getUntrackedParameter<edm::InputTag>("jets"))),
       fileWriterOutputToGT_(l1t::demo::parseFileFormat(iConfig.getUntrackedParameter<std::string>("format")),
                             iConfig.getUntrackedParameter<std::string>("outputFilename"),
-                            kFramesPerTMUXPeriod,
+                            kFramesPerBX,
                             kCTL2BoardTMUX,
                             kMaxLinesPerFile,
-                            kChannelSpecsOutputToGT) {}
+                            kChannelSpecsOutputToGT) {
+      }
 
 void L1CTJetFileWriter::analyze(const edm::Event& iEvent, const edm::EventSetup& iSetup) {
   using namespace edm;
@@ -79,10 +82,16 @@ void L1CTJetFileWriter::endJob() {
 
 std::array<std::vector<ap_uint<64>>, 1> L1CTJetFileWriter::encodeJets(const edm::View<l1t::PFJet>& jets) {
     std::vector<ap_uint<64>> jet_words;
-    std::for_each(jets.begin(), jets.end(), [&](auto jet){
-        jet_words.push_back(jet.encodedJet()[0]);
-        jet_words.push_back(jet.encodedJet()[1]);
-    });
+    for(unsigned i=0; i < nJets; i++){
+        l1t::PFJet j;
+        if(i < jets.size()){
+            j = jets.at(i);
+        }else{ // pad up to nJets with null jets
+            l1t::PFJet j(0,0,0,0,0,0);
+        }
+        jet_words.push_back(j.encodedJet()[0]);
+        jet_words.push_back(j.encodedJet()[1]);
+    }
     std::array<std::vector<ap_uint<64>>, 1> link = {{ jet_words }};
     return link;
 }
--- L1Trigger/Phase2L1ParticleFlow/python/l1ctJetFileWriter_cff.py
+++ L1Trigger/Phase2L1ParticleFlow/python/l1ctJetFileWriter_cff.py
@@ -2,6 +2,10 @@ import FWCore.ParameterSet.Config as cms
 
 l1ctSeededConeJetFileWriter = cms.EDAnalyzer('L1CTJetFileWriter',
   jets = cms.InputTag("scPFL1PuppiEmulator"),
-  outputFilename = cms.untracked.string("L1CTJetsPatterns"),
+  nJets = cms.uint32(12),
+  nFramesPerBX = cms.uint32(9), # 360 MHz clock or 25 Gb/s link
+  TMUX = cms.uint32(6),
+  maxLinesPerFile = cms.uint32(1024),
+  outputFilename = cms.untracked.string("L1CTSCJetsPatterns"),
   format = cms.untracked.string("EMP")
 )
