088689a53066c1dcc80675597db9906f51fad519
Gianluca <gianluca.cerminara@cern.ch>
2021-12-08 22:29:12 +0100

    move some functions back to header...only way to make it compile

--- L1Trigger/Phase2L1ParticleFlow/src/newfirmware/egamma/l2egencoder_ref.cpp
+++ L1Trigger/Phase2L1ParticleFlow/src/newfirmware/egamma/l2egencoder_ref.cpp
@@ -16,35 +16,3 @@ void L2EgEncoderEmulator::toFirmware(const std::vector<ap_uint<64>>& encoded_in,
     encoded_fw[i] = (i < encoded_in.size()) ? encoded_in[i] : ap_uint<64>(0);
   }
 } 
-
-std::vector<ap_uint<64>> L2EgEncoderEmulator::encodeLayer2EgObjs(unsigned int nObj, 
-  const std::vector<EGIsoObjEmu>& photons, 
-  const std::vector<EGIsoEleObjEmu>& electrons) const {
-    std::vector<ap_uint<64>> ret;
-    
-    auto encoded_photons = encodeLayer2(photons);
-    encoded_photons.resize(nObj, {0});
-    auto encoded_eles = encodeLayer2(electrons);
-    encoded_eles.resize(nObj, {0});
-
-    encodeLayer2To64bits(encoded_photons, ret);
-    encodeLayer2To64bits(encoded_eles, ret);
-
-    return ret;
-}
-
-void L2EgEncoderEmulator::encodeLayer2To64bits(const std::vector<ap_uint<96>>& packed96, std::vector<ap_uint<64>>& packed64) const {
-  for(unsigned int i = 0; i < packed96.size(); i+=2) {
-
-    packed64.push_back(packed96[i](63, 0));
-    packed64.push_back((ap_uint<32>(packed96[i+1](95, 64)), ap_uint<32>(packed96[i](95, 64))));
-    packed64.push_back(packed96[i+1](63, 0));
-
-    // std::cout << "obj [" << i << "]: " << std::hex << packed96[i] << std::endl;
-    // std::cout << "obj [" << i+1 << "]: " << std::hex << packed96[i+1] << std::endl;
-    // std::cout << "frame [" << std::dec << packed64.size()-3 << "]" << std::hex << packed64[packed64.size()-3] << std::endl;
-    // std::cout << "frame [" << std::dec << packed64.size()-2 << "]" << std::hex << packed64[packed64.size()-2] << std::endl;
-    // std::cout << "frame [" << std::dec << packed64.size()-1 << "]" << std::hex << packed64[packed64.size()-1] << std::endl;
-    
-  }
-}
--- L1Trigger/Phase2L1ParticleFlow/src/newfirmware/egamma/l2egencoder_ref.h
+++ L1Trigger/Phase2L1ParticleFlow/src/newfirmware/egamma/l2egencoder_ref.h
@@ -31,10 +31,22 @@ namespace l1ct {
       void toFirmware(const std::vector<ap_uint<64>>& encoded_in, ap_uint<64> encoded_fw[]) const;
 
 
-      
       std::vector<ap_uint<64>> encodeLayer2EgObjs(unsigned int nObj, 
         const std::vector<EGIsoObjEmu>& photons, 
-        const std::vector<EGIsoEleObjEmu>& electrons) const;
+        const std::vector<EGIsoEleObjEmu>& electrons) const {
+          std::vector<ap_uint<64>> ret;
+          
+          auto encoded_photons = encodeLayer2(photons);
+          encoded_photons.resize(nObj, {0});
+          auto encoded_eles = encodeLayer2(electrons);
+          encoded_eles.resize(nObj, {0});
+          // 
+          encodeLayer2To64bits(encoded_photons, ret);
+          encodeLayer2To64bits(encoded_eles, ret);
+
+          return ret;
+      }
+
 
     private:
 
@@ -42,11 +54,11 @@ namespace l1ct {
       ap_uint<96> encodeLayer2(const T& egiso) const {
         ap_uint<96> ret = 0;
         // FIXME; should be packed in GT format
-        ret(T::BITWIDTH, 0) = egiso.pack();
+        ret(T::BITWIDTH-1, 0) = egiso.pack();
         return ret;
       }
-
-
+      
+      
       template<class T>
       std::vector<ap_uint<96>> encodeLayer2(const std::vector<T>& egisos) const {
         std::vector<ap_uint<96>> ret;
@@ -55,9 +67,24 @@ namespace l1ct {
         }
         return ret;
       }
-
-      void encodeLayer2To64bits(const std::vector<ap_uint<96>>& packed96, std::vector<ap_uint<64>>& packed64) const;
+      // 
       
+      void encodeLayer2To64bits(const std::vector<ap_uint<96>>& packed96, std::vector<ap_uint<64>>& packed64) const {
+        for(unsigned int i = 0; i < packed96.size(); i+=2) {
+
+          packed64.push_back(packed96[i](63, 0));
+          packed64.push_back((ap_uint<32>(packed96[i+1](95, 64)), ap_uint<32>(packed96[i](95, 64))));
+          packed64.push_back(packed96[i+1](63, 0));
+
+          // std::cout << "obj [" << i << "]: " << std::hex << packed96[i] << std::endl;
+          // std::cout << "obj [" << i+1 << "]: " << std::hex << packed96[i+1] << std::endl;
+          // std::cout << "frame [" << std::dec << packed64.size()-3 << "]" << std::hex << packed64[packed64.size()-3] << std::endl;
+          // std::cout << "frame [" << std::dec << packed64.size()-2 << "]" << std::hex << packed64[packed64.size()-2] << std::endl;
+          // std::cout << "frame [" << std::dec << packed64.size()-1 << "]" << std::hex << packed64[packed64.size()-1] << std::endl;
+
+        }
+      }
+            
       unsigned int nEncodedWords_;
   };
 
