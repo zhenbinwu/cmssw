84cc894789ef65caa40bb0c5736d13071aec230b
Alexx Perloff <Alexx.Perloff@Colorado.edu>
2022-08-14 02:00:53 -0500

    Add new codecs to DemonstratorTools.
    (cherry picked from commit 8f2096b2dc3e7075ed5a2045ee828d486694d5e3)
    (cherry picked from commit 0d81be0f93ae96ff3edb80b7e3eba468ae789734)

--- L1Trigger/DemonstratorTools/src/codecs_htsums.cc
+++ L1Trigger/DemonstratorTools/src/codecs_htsums.cc
@@ -3,56 +3,54 @@
 
 namespace l1t::demo::codecs {
 
-  ap_uint<64> encodeHtSum(const l1t::EtSum& htSum) {
-    l1tmhtemu::EtMiss htMiss;
-    htMiss.Et = htSum.p4().energy();
-    htMiss.Phi = htSum.hwPhi();
-    l1tmhtemu::Et_t HT = htSum.hwPt();
-    ap_uint<1> valid = (htSum.hwQual() > 0);
-    ap_uint<64 - (l1tmhtemu::kHTSize + l1tmhtemu::kMHTSize + l1tmhtemu::kMHTPhiSize + 1)> unassigned = 0;
-    ap_uint<64> htSumWord = (unassigned, HT, htMiss.Phi, htMiss.Et, valid);
-    return htSumWord;
+  ap_uint<64> encodeHtSum(const l1t::EtSum& etSum) {
+
+  	l1tmhtemu::EtMiss etMiss;
+  	etMiss.Et = etSum.p4().energy();
+  	etMiss.Phi = etSum.hwPhi();
+    l1tmhtemu::Et_t HT = etSum.hwPt();
+  	ap_uint<1> valid = (etSum.hwQual() > 0);
+  	ap_uint<64 - (l1tmhtemu::kHTSize + l1tmhtemu::kMHTSize + l1tmhtemu::kMHTPhiSize + 1)> unassigned = 0;
+  	ap_uint<64> etSumWord = (unassigned, HT, etMiss.Phi, etMiss.Et, valid);
+  	return etSumWord;
+
   }
 
-  // Encodes htsum collection onto 1 output link
-  std::array<std::vector<ap_uint<64>>, 1> encodeHtSums(const edm::View<l1t::EtSum>& htSums) {
-    std::vector<ap_uint<64>> htSumWords;
+  // Encodes etsum collection onto 1 output link
+  std::array<std::vector<ap_uint<64>>, 1> encodeHtSums(const edm::View<l1t::EtSum>& etSums) {
+    std::vector<ap_uint<64>> etSumWords;
 
-    for (const auto& htSum : htSums)
-      htSumWords.push_back(encodeHtSum(htSum));
+    for (const auto& etSum : etSums)
+      etSumWords.push_back(encodeHtSum(etSum));
 
     std::array<std::vector<ap_uint<64>>, 1> linkData;
 
     for (size_t i = 0; i < linkData.size(); i++) {
-      // Pad etsum vectors -> full packet length (48 frames, but only 1 htsum max)
-      htSumWords.resize(1, 0);
-      linkData.at(i) = htSumWords;
+      // Pad etsum vectors -> full packet length (48 frames, but only 1 etsum max)
+      etSumWords.resize(1, 0);
+      linkData.at(i) = etSumWords;
     }
 
     return linkData;
   }
 
   std::vector<l1t::EtSum> decodeHtSums(const std::vector<ap_uint<64>>& frames) {
-    std::vector<l1t::EtSum> htSums;
+    std::vector<l1t::EtSum> etSums;
 
     for (const auto& x : frames) {
       if (not x.test(0))
         break;
 
       math::XYZTLorentzVector v(0, 0, 0, l1tmhtemu::MHT_t(x(l1tmhtemu::kMHTSize + 1, 1)).to_int());
-      l1t::EtSum s(
-          v,
-          l1t::EtSum::EtSumType::kMissingHt,
-          l1tmhtemu::Et_t(x(l1tmhtemu::kHTSize + l1tmhtemu::kMHTPhiSize + l1tmhtemu::kMHTSize + 1,
-                            1 + l1tmhtemu::kMHTPhiSize + l1tmhtemu::kMHTSize + 1))
-              .to_int(),
-          0,
-          l1tmhtemu::MHTphi_t(x(l1tmhtemu::kMHTPhiSize + l1tmhtemu::kMHTSize + 1, 1 + l1tmhtemu::kMHTSize + 1)).to_int(),
-          0);
-      htSums.push_back(s);
+      l1t::EtSum s(v,
+                   l1t::EtSum::EtSumType::kMissingHt,
+                   l1tmhtemu::Et_t(x(l1tmhtemu::kHTSize + l1tmhtemu::kMHTPhiSize + l1tmhtemu::kMHTSize + 1, 1 + l1tmhtemu::kMHTPhiSize + l1tmhtemu::kMHTSize + 1)).to_int(),
+                   0,
+                   l1tmhtemu::MHTphi_t(x(l1tmhtemu::kMHTPhiSize + l1tmhtemu::kMHTSize + 1, 1 + l1tmhtemu::kMHTSize + 1)).to_int(), 0);
+      etSums.push_back(s);
     }
 
-    return htSums;
+    return etSums;
   }
 
 }  // namespace l1t::demo::codecs
