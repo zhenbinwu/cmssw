e13c8d8f4f7a71d9678e93d87918aac37ac7c80a
Martin Grunewald <Martin.Grunewald@cern.ch>
2024-02-02 11:11:25 +0100

    Cleanup of ../tables and adding OnlineCosmics files

--- HLTrigger/Configuration/test/OnLine_HLT_GRun.py
+++ HLTrigger/Configuration/test/OnLine_HLT_GRun.py
@@ -1,6 +1,6 @@
 # hltGetConfiguration /dev/CMSSW_13_3_0/GRun --full --data --type GRun --unprescale --process HLTGRun --globaltag auto:run3_hlt_GRun --input file:RelVal_Raw_GRun_DATA.root
 
-# /dev/CMSSW_13_3_0/GRun/V19 (CMSSW_13_3_0)
+# /dev/CMSSW_13_3_0/GRun/V21 (CMSSW_13_3_0)
 
 import FWCore.ParameterSet.Config as cms
 
@@ -12,7 +12,7 @@ process = cms.Process( "HLTGRun" )
 process.ProcessAcceleratorCUDA = ProcessAcceleratorCUDA()
 
 process.HLTConfigVersion = cms.PSet(
-  tableName = cms.string('/dev/CMSSW_13_3_0/GRun/V19')
+  tableName = cms.string('/dev/CMSSW_13_3_0/GRun/V21')
 )
 
 process.HLTIter0PSetTrajectoryBuilderIT = cms.PSet( 
