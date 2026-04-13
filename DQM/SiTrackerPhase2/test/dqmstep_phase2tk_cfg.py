f29801a292cf46de4fc2ea2fde6731984d39f268
Brandi Skipworth <bskipwo1@vols.utk.edu>
2026-04-13 19:15:33 +0200

    comment out validation step as per alaperto request

--- DQM/SiTrackerPhase2/test/dqmstep_phase2tk_cfg.py
+++ DQM/SiTrackerPhase2/test/dqmstep_phase2tk_cfg.py
@@ -35,7 +35,7 @@ process.load('DQMOffline.Configuration.DQMOffline_cff')
 process.load('Configuration.StandardSequences.FrontierConditions_GlobalTag_cff')
 
 process.maxEvents = cms.untracked.PSet(
-    input = cms.untracked.int32(10),
+    input = cms.untracked.int32(-1),
     output = cms.optional.untracked.allowed(cms.int32,cms.PSet)
 )
 
